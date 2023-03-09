11bb1566fd59f663832ca17cceb35e84721974c6
JRatchell <jrboston1@gmail.com>
2023-03-09 08:27:00 -0500

    The definition of a barebones commit, genuinely need to do better with this

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,15 @@
 function WIP (msg) {
     alert(msg);
 }
+
+function accountIntent (msg) {
+    alert(msg)
+}
+
+function getStartedIntent (msg) {
+    alert(msg)
+}
+
+function indexIntent (msg) {
+    alert(msg)
+}
