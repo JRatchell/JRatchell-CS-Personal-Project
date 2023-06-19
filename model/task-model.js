00689ffb2b856ea4ce7f28255aa15094fed551fe
JRatchell <jrboston1@gmail.com>
2023-06-19 16:15:55 -0400

    added framework to task-model.js

--- model/task-model.js
+++ model/task-model.js
@@ -1,1 +1,22 @@
-a
+const { Schema, default: mongoose } = require("mongoose");
+
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, "This information is required"],
+        
+    },
+    author: {
+        type: String,
+        required: [true, "This information is required"],
+        
+    },
+    task: {
+        type: String,
+        required: [true, "This information is required"],
+        
+    },
+});
+const Task = mongoose.model("Task", taskSchema)
+module.exports = Task
