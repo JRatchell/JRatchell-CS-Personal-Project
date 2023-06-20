83ef355323666c4b1112ba05f18b85cf19933c6c
JRatchell <jrboston1@gmail.com>
2023-06-20 15:50:18 -0400

    css is fixed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,4 @@
 confirm("the ejs is working if you can see this")
-function WIP (msg) {
-    alert(msg);
-}
-
-function accountIntent (msg) {
-    alert(msg)
-}
-
-function getStartedIntent (msg) {
-    alert(msg)
-}
-
-function indexIntent (msg) {
-    alert(msg)
-}
 
 // Hamburger Menu
 // DOM Event Steps:
