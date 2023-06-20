2d6261c4e22db6b1d87c71f5a28eed812d91de0b
JRatchell <jrboston1@gmail.com>
2023-06-20 01:06:46 -0400

    mongoDB setup

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+const methodOverride = require('method-override')
 // this is where our entry point file is, middle ware, express and server
 const express = require('express');
 //const { request } = require('https');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Morgan Functionality 
 const morgan = require('morgan');
@@ -18,10 +20,22 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.use(session({
+    secret: process.env.SECRET_KEY, // note that secrets are mandatory
+    resave: false, 
+    saveUninitialized: false
+}));
+
+// initialize passport
+app.use(passport.initialize());
+
+// passport to use session
+app.use(passport.session());
+
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
 
-
+require('./config/connection')
 //Server
 app.listen(PORT, () => {
     console.log(`Kit and Joshua's server is ready and all ears on port ${PORT}!`);
